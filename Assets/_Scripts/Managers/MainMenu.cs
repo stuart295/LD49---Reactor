6dae626115b573385b4f50bdc4b71fc5ac8cb9e7
Stuart <stuart295@gmail.com>
2021-10-03 18:25:30 +0200

    Added main menu

---
+++ Assets/_Scripts/Managers/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GetComponent<GameManager>();
+        gm.Play();
+    }
+
+    
+
+    public void StartGame() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Sandbox() {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
